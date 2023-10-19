9ab75cb3e9b490da2268903e9e2de7af2c512ea7
Ruslan <89604587433@mail.ru>
2023-10-19 20:27:20 +0300

    TaskHome 6

---
+++ project_vue_cli/src/App.vue
@@ -0,0 +1,42 @@
+<template> 
+  
+<headerSight/>
+<mainFirstPage/>
+<footerSight/>
+<hr>
+<headerSight/>
+<mainTwoPage/>
+<footerSight/>
+<hr>
+<headerSight/>
+<mainThirdPage/>
+<footerSight/>
+<hr> 
+<headerSight/>
+<MainForthPage/>
+<footerSight/>
+
+</template>
+
+<script>
+import headerSight from './components/headerSight.vue';
+import mainTwoPage from './components/mainTwoPage.vue';
+import mainFirstPage from './components/mainFirstPage.vue';
+import mainThirdPage from './components/mainThirdPage.vue';
+import footerSight from './components/footerSight.vue';
+import MainForthPage from './components/mainForthPage.vue';
+export default {  
+  components: {
+    headerSight,
+    footerSight,
+    mainTwoPage,
+    mainFirstPage,
+    mainThirdPage,
+    MainForthPage
+}
+}
+</script>
+
+<style lang="scss">
+
+</style>
